0c90f604bc673ee9ee295d68f2b51c7f39d31b7a
GSVasconcelos <cabrudev@gmail.com>
2026-04-10 15:16:05 -0300

    Criando arquivos python

---
+++ python-files/concat_strings.py
@@ -0,0 +1,4 @@
+nome1 = input("Digite o primeiro nome: ")
+nome2 = input("Digite o segundo nome: ")
+
+print("Os nomes concatenados fica assim: " + nome1 + " " + nome2)
---
+++ python-files/rep_strings.py
@@ -0,0 +1,4 @@
+nome = input("Digite uma palavra: ")
+repeticoes = int(input("Digite o número de repetições para " + nome + ": "))
+
+print((nome + " ") * repeticoes)
